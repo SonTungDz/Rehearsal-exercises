114c01c077fb7327b8d230463670b3e7cbcb978e
SonTungDz <yayass307@gmail.com>
2024-06-29 20:49:47 +0700

    ex5 reha.cpp

--- ex5 reha.cpp
+++ ex5 reha.cpp
@@ -1,7 +1,22 @@
 #include<stdio.h>
+#include<math.h>
+// Ex 5
 
-
+int checkevenodd(int n){
+        if(n % 2 != 0){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
 int main(){
-	printf("Hello ");
-	return 0;
+    int n;
+    scanf("%d", &n);
+    if(checkevenodd(n)){
+        printf("odd");
+    } else{
+        printf("even");
+    }
+    return 0;
 }
